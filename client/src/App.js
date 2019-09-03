d862aecb43833924a45ffabc7e451659829edf4f
Ghostman100 <gostanin75@gmail.com>
2019-09-03 23:29:48 +0700

    add style to client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+import './css/reboot.css';
+import './css/bootstrap-grid.css';
+import './css/main.css';
 import {BeatLoader} from 'react-spinners';
 import {YMaps, Map, Placemark} from 'react-yandex-maps';
 
@@ -72,28 +74,20 @@ class App extends React.Component {
                                                 hasHint: true
                                             }}
                                         />,
-                                        list: <tr key={place.properties.CompanyMetaData.id}>
-                                            <td>
-                                                <input
-                                                    type={'checkbox'}
-                                                    onChange={this.handleCheck}
-                                                    name={index}
-                                                    // value={}
-                                                />
-                                            </td>
-                                            <td>
-                                                {place.properties.CompanyMetaData.name}
-                                            </td>
-                                            <td>
-                                                {text}
-                                            </td>
-                                            <td>
-                                                {city}
-                                            </td>
-                                            <td>
-                                                {place.properties.CompanyMetaData.address}
-                                            </td>
-                                        </tr>
+                                        list:
+                                            <tr key={place.properties.CompanyMetaData.id}>
+                                                <td className="result-check"><label><input type="checkbox"
+                                                                                           onChange={this.handleCheck}
+                                                                                           name={index}/><span></span></label>
+                                                </td>
+                                                <td className="result-name">{place.properties.CompanyMetaData.name}</td>
+                                                <td className="result-type">{text}</td>
+                                                <td className="result-city">{city}</td>
+                                                <td className="result-street">{place.properties.CompanyMetaData.address}
+                                                </td>
+                                                {/*<td className="result-house">82</td>*/}
+                                                {/*<td className="result-housing">-</td>*/}
+                                            </tr>
                                     }
 
                                 )
@@ -172,53 +166,157 @@ class App extends React.Component {
 
     render() {
         return (
-            <div style={{flex: 1, height: '100%', borderWidth: 10, borderColor: 'red'}}>
-                <form>
-                    Город <input type={'text'} value={this.state.city}
-                                 onChange={(event) => this.setState({city: event.target.value})}/>
-                    <br/>
-                    Адрес <input type={'text'} value={this.state.address}
-                                 onChange={(event) => this.setState({address: event.target.value})}/>
-                    <br/>
-                    Поиск <input type={'text'} value={this.state.name}
-                                 onChange={(event) => this.setState({name: event.target.value})}/>
-                    <br/>
-                    <input type="button" value="Применить"
-                           onClick={() => this.handleSearch(this.state.city, this.state.address, this.state.name)}/>
-                </form>
-                <YMaps>
-                    <div style={{height: '100%'}}>
-                        My awesome application with maps!
-                        <Map
-                            width={'100%'}
-                            height={400}
-                            state={this.state}
-                        >
-                            {this.state.places && this.state.places.map((place) => place.marker)}
-                        </Map>
+            <div>
+                <div className="toolbar">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-3 col-md-4">
+                                <div className="logo"><a href="#"><img src={require("./img/logo.png")} alt="logo"/></a>
+                                </div>
+                            </div>
+                            <div className="col-lg-9 col-md-8">
+                                <div className="toolbar-row">
+                                    <div className="toolbar__navigation">
+                                        <ul className="toolbar__tabs">
+                                            <li className="selected"><a href="#">Места</a></li>
+                                            <li><a href="#">Пользователи</a></li>
+                                            <li><a href="#">Жалобы</a></li>
+                                        </ul>
+                                    </div>
+                                    <div className="toolbar__login">
+                                        <ul className="toolbar__user">
+                                            <li><a href="#"><img src={require("./img/toolbar__icons.png")}
+                                                                 alt="img"/></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </YMaps>
-                <p>
-                    Выбрано {this.state.checkCounter} объектов
-                </p>
-                <input type={'button'} value="Добавить" onClick={this.handleSubmit}/>
-                {this.state.process ? <BeatLoader
-                    sizeUnit={"px"}
-                    size={150}
-                    color={'#FC3067'}
-                    loading={this.state.process}
-                /> : <table style={{flex: 1, justifyContent: 'space-between'}}>
-                    <tbody>
-                    <tr>
-                        <th></th>
-                        <th>Назавание</th>
-                        <th>Тип</th>
-                        <th>Город</th>
-                        <th>Адрес</th>
-                    </tr>
-                    {this.state.places && this.state.places.map((place) => place.list)}
-                    </tbody>
-                </table>}
+                </div>
+
+                <div className="tabs__content">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <div className="place-filter">
+                                    <form action="/">
+
+                                        <div className="filter-btns">
+                                            <button className="add-btn" type="button">Добавить</button>
+                                            <button className="added-btn" type="button">Добавленные</button>
+                                        </div>
+
+                                        <div className="form-title">Фильтр поиска мест</div>
+
+                                        <div className="form-item">
+                                            <div>Город</div>
+                                            <input type={'text'} value={this.state.city}
+                                                   onChange={(event) => this.setState({city: event.target.value})}/>
+                                        </div>
+
+                                        <div className="form-item">
+                                            <div>Адрес</div>
+                                            <input type={'text'} value={this.state.address}
+                                                         onChange={(event) => this.setState({address: event.target.value})}/>
+                                        </div>
+
+                                        {/*<div className="form-item">*/}
+                                        {/*    <div className="filter-item">Улица</div>*/}
+                                        {/*    <select name="street">*/}
+                                        {/*        <option value="1">Большой проспект П.С.</option>*/}
+                                        {/*        <option value="2">Ленина</option>*/}
+                                        {/*    </select>*/}
+                                        {/*</div>*/}
+
+                                        {/*<div className="form-row">*/}
+                                        {/*    <div className="form-item">*/}
+                                        {/*        <div className="filter-item">Дом</div>*/}
+                                        {/*        <select name="house">*/}
+                                        {/*            <option value="1">1</option>*/}
+                                        {/*            <option value="2">2</option>*/}
+                                        {/*        </select>*/}
+                                        {/*    </div>*/}
+
+                                        {/*    <div className="form-item">*/}
+                                        {/*        <div className="filter-item">Корпус</div>*/}
+                                        {/*        <select name="housing">*/}
+                                        {/*            <option value="1">1</option>*/}
+                                        {/*            <option value="2">2</option>*/}
+                                        {/*        </select>*/}
+                                        {/*    </div>*/}
+                                        {/*</div>*/}
+
+                                        <div className="form-item">
+                                            <div >Тип заведения</div>
+                                            <input type={'text'} value={this.state.name}
+                                                   onChange={(event) => this.setState({name: event.target.value})}/>
+                                        </div>
+                                        <input className="form-btn" type={'button'} value="Применить"
+                                               onClick={() => this.handleSearch(this.state.city, this.state.address, this.state.name)}/>
+                                        {/*<button className="form-btn" type="submit"></button>*/}
+                                    </form>
+                                </div>
+
+                                <div className="result-filter">
+                                    <div
+                                        className="result-filter_txt">Выбрано <span>{this.state.checkCounter}</span> объектов
+                                    </div>
+                                    <button className="btn btn-blue" type="button"
+                                            onClick={this.handleSubmit}>Добавить
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="col-lg-8">
+                                <div className="tabs-map">
+                                    <div className="map">
+                                        <YMaps>
+                                            <div>
+                                                <Map
+                                                    width={'100%'}
+                                                    height={440}
+                                                    state={this.state}
+                                                >
+                                                    {this.state.places && this.state.places.map((place) => place.marker)}
+                                                </Map>
+                                            </div>
+                                        </YMaps>
+                                    </div>
+                                    {/*<img src="img/map.png" alt="img">*/}
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="tabs__table">
+                            {this.state.process ? <BeatLoader
+                                    sizeUnit={"px"}
+                                    size={150}
+                                    color={'#FC3067'}
+                                    loading={this.state.process}
+                                /> :
+                                <table className="table result-table">
+                                    <thead>
+                                    <tr>
+                                        <td className="result-check"><label><input
+                                            type="checkbox"/><span></span></label>
+                                        </td>
+                                        <td className="result-name">Название</td>
+                                        <td className="result-type">ТИП</td>
+                                        <td className="result-city">Город</td>
+                                        <td className="result-street">Адрес</td>
+                                        {/*<td className="result-house">ДОМ</td>*/}
+                                        {/*<td className="result-housing">Корпус</td>*/}
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    {this.state.places && this.state.places.map((place) => place.list)}
+                                    </tbody>
+                                </table>}
+
+                            <div className="show-link"><a href="#" className="btn btn-pink">Просмотреть</a></div>
+                        </div>
+                    </div>
+                </div>
             </div>)
     }
 }
